14a7c36664bfe7a5dbf95996cc3225c48b93c414
Alex Da Corte <dacortejhexson@gmail.com>
2020-07-20 11:41:57 -0500

    Updates

---
+++ precache-manifest.bfd2a56b2f7cf55b048928e785966721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fc30471c57c7211a55c",
+    "url": "/mypkdex/static/css/main.6595298e.chunk.css"
+  },
+  {
+    "revision": "5fc30471c57c7211a55c",
+    "url": "/mypkdex/static/js/main.5fc30471.chunk.js"
+  },
+  {
+    "revision": "9b53553a90833d0b7d8a",
+    "url": "/mypkdex/static/css/1.d159feba.chunk.css"
+  },
+  {
+    "revision": "9b53553a90833d0b7d8a",
+    "url": "/mypkdex/static/js/1.9b53553a.chunk.js"
+  },
+  {
+    "revision": "abaad7a1d5917a57abf6",
+    "url": "/mypkdex/static/js/runtime~main.abaad7a1.js"
+  },
+  {
+    "revision": "d8cee4c919bc67fd14a3d7e9081938e5",
+    "url": "/mypkdex/index.html"
+  }
+];
